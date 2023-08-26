c7819e20b324c048cafcc0a408ac8acf09ec2a72
estealvarez <este.alvarez@duocuc.cl>
2023-08-26 01:02:01 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer container">
+	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'blog' ) ); ?>">
 				<?php
