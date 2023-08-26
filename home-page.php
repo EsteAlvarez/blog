06b4511fc005e93a9884e89078fecae4c7828753
estealvarez <este.alvarez@duocuc.cl>
2023-08-26 00:51:21 -0400

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main container-fluid">
+	<main id="primary" class="site-main container-fluid m-0 p-3">
 		<?php
 		while ( have_posts() ) :
 			the_post();
