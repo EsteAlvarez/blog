3e33ff645c8ca89052d9c6fc1d88f3b85f93de2c
estealvarez <este.alvarez@duocuc.cl>
2023-11-11 23:09:10 -0300

    home
    wp_query

--- header.php
+++ header.php
@@ -20,12 +20,12 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class('m-0'); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'blog' ); ?></a>
 
-	<header id="masthead" class="site-header container-fluid p-3">
+	<header id="masthead" class="site-header container">
 		<!-- <div class="site-branding">
 			<?php the_custom_logo(); ?>
 		</div> -->
--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -9,5 +9,77 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
+    <div class="row mt-4">
+		<div class="col-md-12 mb-5">
+			<h2><?php echo the_field('encabezado_de_bienvenida');?></h2>
+			<p class="mt-3"><?php echo the_field('');?></p>
+		</div>
+    </div>
+	<div class="row">
+        <h2 class="mb-3">Clases de JavaScript Avanzado</h2>
+        <!--Consulta por entradas de clases de js-->
+        <?php
+        $args = array(
+        'category_name' => 'clases-de-js-avanzado',
+        'posts_per_page' => -1,
+        'order' => 'ASC',
+        );
+        $the_query = new WP_Query($args);
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post(); ?>
+                <!--Contenido html-->
+                <div class="col-lg-4 col-md-6 mb-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h3 class="card-title fw-bold"><?php echo get_the_title();?></h3>
+                                <p class="card-text"><?php echo get_the_excerpt();?></p>
+                                <a href="<?php the_permalink();?>" class="card-link">Ver Más</a>
+                            </div>
+                        </div>
+                </div>
+                <!--Contenido html-->
+            <?php }
+        } else {
+            echo "no hay resultado para la consulta";
+        }
+        /* Restore original Post Data */
+        wp_reset_postdata();
+        ?>
+        <!--Consulta por entradas de clases de js-->
+	</div>
+	<div class="row mt-5 mb-5">
+        <h2 class="mb-3">Reflexiones</h2>
+        <!--Consulta por entradas de clases de js-->
+        <?php
+        $args = array(
+        'category_name' => 'reflexiones',
+        'posts_per_page' => -1,
+        'order' => 'ASC',
+        );
+        $the_query = new WP_Query($args);
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post(); ?>
+                <!--Contenido html-->
+                <div class="col-lg-4 col-md-6 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title fw-bold"><?php echo get_the_title();?></h3>
+                                <p class="card-text"><?php echo get_the_excerpt();?></p>
+                                <a href="<?php the_permalink();?>" class="card-link">Ver Más</a>
+                            </div>
+                        </div>
+                </div>
+                <!--Contenido html-->
+            <?php }
+        } else {
+            echo "no hay resultado para la consulta";
+        }
+        /* Restore original Post Data */
+        wp_reset_postdata();
+        ?>
+        <!--Consulta por entradas de clases de js-->
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
