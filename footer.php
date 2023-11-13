24d707aa4501ec838c48a82aa3189dce84e6dd91
estealvarez <este.alvarez@duocuc.cl>
2023-11-12 23:15:31 -0300

    estilos

--- footer.php
+++ footer.php
@@ -16,6 +16,8 @@
 			<div class="row text-center p-3">
 				<hr class="linea-footer">
 				<p><?php the_field('nombre_de_autor');?></p>
+				<div class="d-flex justify-content-center">Foto de <a class="ms-1 me-1 link-atribucion text-decoration-none" href="https://unsplash.com/es/@ilyapavlov?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ilya Pavlov</a> en <a class="ms-1 me-1 link-atribucion text-decoration-none" href="https://unsplash.com/es/fotos/monitor-que-muestra-programacion-java-OqtafYT5kTw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></div>
+				
 			</div>
 		</div>
 	</footer><!-- #colophon -->
