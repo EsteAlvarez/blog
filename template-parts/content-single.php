7590e139883bd042a5f128e6e8206020191e35eb
estealvarez <este.alvarez@duocuc.cl>
2023-11-21 23:19:43 -0300

    Update content-single.php

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,8 +9,8 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
-	<div class="row mt-5 p-md-0 p-2">
+<article id="post-<?php the_ID(); ?>" <?php post_class('container p-md-0 p-2'); ?>>
+	<div class="row mt-5">
 		<div style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID));?>'); background-position:center; background-size:cover; background-blend-mode: multiply; background-color: rgba(0, 0, 0, 0.8)" class="col-md-12 mb-5 fondo-encabezado rounded p-5">
             <h2><?php echo the_field('encabezado_de_bienvenida');?></h2>
 			<p class="mt-3"><?php echo the_field('');?><?php the_field('mensaje_de_bienvenida');?></p>
