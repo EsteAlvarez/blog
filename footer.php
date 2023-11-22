581569eb6323a7c5be09548600489adf74fa6ad7
estealvarez <este.alvarez@duocuc.cl>
2023-11-21 21:53:40 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		<div class="container p-1">
 			<div class="row text-center p-3 m-0">
 				<p><?php echo 'Autor del tema: '; the_author();?></p>
-				<hr class="linea-footer w-50 p-0">
+				<hr class="linea-footer m-auto w-50 p-0">
 				<span class="d-flex justify-content-center">Foto de <a class="ms-1 me-1 link-atribucion text-decoration-none" href="https://unsplash.com/es/@ilyapavlov?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ilya Pavlov</a> en <a class="ms-1 me-1 link-atribucion text-decoration-none" href="https://unsplash.com/es/fotos/monitor-que-muestra-programacion-java-OqtafYT5kTw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></span>
 			</div>
 		</div>
